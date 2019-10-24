68f9c36ddc1187479d54e6a3a995b16bd2cb3ee8
kls6378 <kls6378@naver.com>
2019-10-24 10:11:12 +0900

    Fix register mysql error

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const passport = require('passport')
+const mysql = require('mysql')
+const dbConfig = require('../config/db_config')
+const connection = mysql.createConnection(dbConfig)
 const router = express.Router();
 
 router.get('/', (req, res) => {
